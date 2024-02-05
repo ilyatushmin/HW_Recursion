4e9bfcf3cc4ef58bdbe3e97c2073a9681257e3a6
IlyaTush <ilyatushmin@gmail.com>
2024-02-05 19:40:01 +0300

    1

---
+++ 1/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 1: Задайте значения M и N.
+// Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+// Использовать рекурсию, не использовать циклы.
+
+
+
+// 1. Задать глобальные переменные :)
+int M = 2;
+int N = 9;
+
+
+// 2. Составить функцию с рекурсией. Включая условие, если M>N или если N>M.
+string GetNumsFromMtoN(int num1, int num2)
+{
+    if (num1 > num2)
+    {
+        if (num1 == num2)
+        {
+            return num2.ToString();
+        }
+        return num1.ToString() + ", " + GetNumsFromMtoN(num1 - 1, num2);
+    }
+    else
+    {
+        if (num2 == num1)
+        {
+            return num1.ToString();
+        }
+        return num2.ToString() + ", " + GetNumsFromMtoN(num1, num2 - 1);
+    }
+
+}
+
+// 3. Вывод результата:
+Console.WriteLine(GetNumsFromMtoN(M, N));
---
+++ 3/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 3: Задайте произвольный массив.
+// Выведете его элементы, начиная с конца.
+// Использовать рекурсию, не использовать циклы.
+
+
+// 1. Функция по созданию массива из случайных целых чисел (+вывод):
+int[] CreateArray(int ArrayLength)
+{
+    int[] Arr = new int[ArrayLength];
+    for (int i = 0; i < Arr.Length; i++)
+    {
+        Arr[i] = new Random().Next(1, 101);
+        Console.Write($"{Arr[i]} ");
+    }
+    Console.WriteLine();
+    return Arr;
+}
+
+// 2. Функция по выводу массива в обратном порядке с исп. рекурсии:
+void ReverseAndPrint(int[] Array, int i)
+{
+    if (i != 0)
+    {
+        Console.Write($"{Array[i-1]} ");
+        ReverseAndPrint(Array, i-1);
+    }
+}
+
+
+// 3. Вызов функций и указание длины массива:
+int[] Result = CreateArray(10);
+ReverseAndPrint(Result, 10);
+
